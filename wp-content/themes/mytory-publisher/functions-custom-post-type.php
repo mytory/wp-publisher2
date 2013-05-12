b478a69a0b751fe2a1eab873824d20da78c6c7a6
mytory <mytory@gmail.com>
2013-05-12 22:35:34 +0900

    책 이름표 상세화

--- wp-content/themes/mytory-publisher/functions-custom-post-type.php
+++ wp-content/themes/mytory-publisher/functions-custom-post-type.php
@@ -2,8 +2,25 @@
 //===== book custom post type =====
 
 function mpub_custom_post_type() {
+
+  $labels = array(
+    'name' => '책',
+    'singular_name' => '책',
+    'add_new' => '책 추가',
+    'add_new_item' => '책 추가',
+    'edit_item' => '책 수정',
+    'new_item' => '책 추가',
+    'all_items' => '책 목록',
+    'view_item' => '책 상세 보기',
+    'search_items' => '책 검색',
+    'not_found' =>  '등록된 책이 없습니다',
+    'not_found_in_trash' => '휴지통에 책이 없습니다',
+    'parent_item_colon' => '부모 책:',
+    'menu_name' => '책',
+  );
+
   $args = array(
-    'label' => "책",
+    'labels' => $labels,
     'public' => true,
     'has_archive' => true,
   );
