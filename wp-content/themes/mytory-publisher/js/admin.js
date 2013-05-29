c559efa6ca94153466846fdeb6f2a414deb0b9e6
mytory <mytory@gmail.com>
2013-05-30 03:28:58 +0900

    bind event to number type input

--- wp-content/themes/mytory-publisher/js/admin.js
+++ wp-content/themes/mytory-publisher/js/admin.js
@@ -1,5 +1,15 @@
 jQuery(document).ready(function($){
+
+  // 브라우저가 달력을 제공하지 않는 경우 달력 호출
   if( ! Modernizr.inputtypes.date){
     $('input[type="date"]').datepicker();
+  }
+
+  // 숫자만 남기고 지우기
+  if( ! Modernizr.inputtypes.number){
+    $('input[type="number"]').blur(function(){
+      var new_val = $(this).val().replace(/[^0-9]/g, '');
+      $(this).val(new_val);
+    });
   }
 });
